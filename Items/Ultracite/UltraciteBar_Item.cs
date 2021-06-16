8f4f663822a23aca12469c656f0fa875a4ee5e8a
James <murphybaumanis@gmail.com>
2021-06-16 16:37:17 +1000

    Added UltraciteBar and tile
    new Ultracite bar and tile + crafting recipe Ancient Manipulator(temporary).

---
+++ Items/Ultracite/UltraciteBar_Item.cs
@@ -0,0 +1,54 @@
+using Microsoft.Xna.Framework;
+using IL.Terraria.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace wartinyfall76.Items.Ultracite
+{
+    public class UltraciteBar_Item : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ultracite Bar");
+            Tooltip.SetDefault("Refined Ultracite. \nIs radioactive, handle with caution.");
+        }
+
+        public override void SetDefaults()
+        {
+            // hitbox
+            item.width = 20;
+            item.height = 20;
+            
+            item.maxStack = 999;
+            item.value = 18000;
+            item.rare = 11;
+            item.consumable = true;
+            // Set other item.X values here
+            // Usage
+            item.useTime = 12;
+            item.useAnimation = 12;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.autoReuse = true;
+            //create tile
+            item.createTile = ModContent.TileType<Tiles.Ultracite.UltraciteBar_Tile>();
+
+
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.UltraciteOre_Item, 6);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+            // Recipes here. See Basic Recipe Guide
+        }
+    }
+}
